d51e2821a28853838ae9db3213079832ffdc67fe
HansPeterMeier <32676715+HansPeterMeier@users.noreply.github.com>
2019-06-02 20:55:54 +0200

    Update main.js

--- main.js
+++ main.js
@@ -36,9 +36,9 @@ var connected = false;
 var points = {
     'SMASerial': 	        {offset: 20,  length: 4, factor: 1, unit: ''},
     'pregard':	                {offset: 32,  length: 4, factor: 1 / 10, unit: 'Watt'},
-    'pregardcounter':           {offset: 40,  length: 8, factor: 1 / 3600000, unit: 'kWh'},
+    'pregardcounter':           {offset: 40,  length: 6, factor: 1 / 3600000, unit: 'kWh'},
     'psurplus':                 {offset: 52,  length: 4, factor: 1 / 10, unit: 'Watt'},
-    'psurpluscounter':          {offset: 60,  length: 8, factor: 1 / 3600000, unit: 'kWh'}
+    'psurpluscounter':          {offset: 60,  length: 6, factor: 1 / 3600000, unit: 'kWh'}
 };
 
 function readData() {
@@ -113,30 +113,30 @@ function readData() {
 function main() {
     if (adapter.config.ext && adapter.config.ext === true) {   
     points['qregard'] =                  {offset: 72,  length: 4, factor: 1 / 10, unit: 'var'};
-    points['qregardcounter'] =       	 {offset: 80,  length: 8, factor: 1 / 3600000, unit: 'varh'};
+    points['qregardcounter'] =       	 {offset: 80,  length: 6, factor: 1 / 3600000, unit: 'varh'};
     points['qsurplus'] =                 {offset: 92,  length: 4, factor: 1 / 10, unit: 'var'};
-    points['qsurpluscounter'] =          {offset: 100, length: 8, factor: 1 / 3600000, unit: 'varh'};
+    points['qsurpluscounter'] =          {offset: 100, length: 6, factor: 1 / 3600000, unit: 'varh'};
     points['sregard'] =                  {offset: 112, length: 4, factor: 1 / 10, unit: 'VA'};
-    points['sregardcounter'] =           {offset: 120, length: 8, factor: 1 / 3600000, unit: 'VAh'};
+    points['sregardcounter'] =           {offset: 120, length: 6, factor: 1 / 3600000, unit: 'VAh'};
     points['ssurplus'] =                 {offset: 132, length: 4, factor: 1 / 10, unit: 'VA'};
-    points['ssurpluscounter'] =          {offset: 140, length: 8, factor: 1 / 3600000, unit: 'VAh'};
+    points['ssurpluscounter'] =          {offset: 140, length: 6, factor: 1 / 3600000, unit: 'VAh'};
     points['cosphi'] =                   {offset: 152, length: 4, factor: 1/ 1000, unit: 'Φ'};
 };
 
  if (adapter.config.L1 && adapter.config.L1 === true) {   
     points['L1.p1regard'] =              {offset: 160, length: 4, factor: 1/ 10, unit: 'Watt'};
-    points['L1.p1regardcounter'] =	 {offset: 168, length: 8, factor: 1/ 3600000, unit: 'kWh'};
+    points['L1.p1regardcounter'] =	 {offset: 168, length: 6, factor: 1/ 3600000, unit: 'kWh'};
     points['L1.p1surplus'] =             {offset: 180, length: 4, factor: 1/ 10, unit: 'Watt'};
-    points['L1.p1surpluscounter'] =      {offset: 188, length: 8, factor: 1/ 3600000, unit: 'kWh'};
+    points['L1.p1surpluscounter'] =      {offset: 188, length: 6, factor: 1/ 3600000, unit: 'kWh'};
  if (adapter.config.ext && adapter.config.ext === true) { 
     points['L1.q1regard'] =              {offset: 200, length: 4, factor: 1/ 10, unit: 'var'};
-    points['L1.q1regardcounter'] =       {offset: 208, length: 8, factor: 1/ 3600000, unit: 'var'};
+    points['L1.q1regardcounter'] =       {offset: 208, length: 6, factor: 1/ 3600000, unit: 'var'};
     points['L1.q1surplus'] =             {offset: 220, length: 4, factor: 1/ 10, unit: 'var'};
-    points['L1.q1surpluscounter'] =      {offset: 228, length: 8, factor: 1/ 3600000, unit: 'var'};
+    points['L1.q1surpluscounter'] =      {offset: 228, length: 6, factor: 1/ 3600000, unit: 'var'};
     points['L1.s1regard'] =              {offset: 240, length: 4, factor: 1/ 10, unit: 'VA'};
-    points['L1.s1regardcounter'] =       {offset: 248, length: 8, factor: 1/ 3600000, unit: 'VAh'};
+    points['L1.s1regardcounter'] =       {offset: 248, length: 6, factor: 1/ 3600000, unit: 'VAh'};
     points['L1.s1surplus'] =             {offset: 260, length: 4, factor: 1/ 10, unit: 'VA'};
-    points['L1.s1surpluscounter'] =      {offset: 268, length: 8, factor: 1/ 3600000, unit: 'VAh'};
+    points['L1.s1surpluscounter'] =      {offset: 268, length: 6, factor: 1/ 3600000, unit: 'VAh'};
     points['L1.cosphi1'] =               {offset: 296, length: 4, factor: 1/ 1000, unit: 'Φ'};
     points['L1.amperage'] =                  {offset: 280, length: 4, factor: 1/ 1000, unit: 'A'};
     points['L1.v1'] =			 {offset: 288, length: 4, factor: 1/ 1000, unit: 'V'};
@@ -145,18 +145,18 @@ function main() {
  
  if (adapter.config.L2 && adapter.config.L2 === true) {
     points['L2.p2regard'] =          	{offset: 304, length: 4, factor: 1/ 10, unit: 'Watt'};
-    points['L2.p2regardcounter'] =   	{offset: 312, length: 8, factor: 1/ 3600000, unit: 'kWh'};
+    points['L2.p2regardcounter'] =   	{offset: 312, length: 6, factor: 1/ 3600000, unit: 'kWh'};
     points['L2.p2surplus'] =         	{offset: 324, length: 4, factor: 1/ 10, unit: 'Watt'};
-    points['L2.p2surpluscounter'] =  	{offset: 332, length: 8, factor: 1/ 3600000, unit: 'kWh'};
+    points['L2.p2surpluscounter'] =  	{offset: 332, length: 6, factor: 1/ 3600000, unit: 'kWh'};
 if (adapter.config.ext && adapter.config.ext === true) { 
     points['L2.q2regard'] =          	{offset: 344, length: 4, factor: 1/ 10, unit: 'var'};
-    points['L2.q2regardcounter'] =   	{offset: 352, length: 8, factor: 1/ 3600000, unit: 'varh'};
+    points['L2.q2regardcounter'] =   	{offset: 352, length: 6, factor: 1/ 3600000, unit: 'varh'};
     points['L2.q2surplus'] =         	{offset: 364, length: 4, factor: 1/ 10, unit: 'var'};
-    points['L2.q2surpluscounter'] =  	{offset: 372, length: 8, factor: 1/ 3600000, unit: 'varh'};
+    points['L2.q2surpluscounter'] =  	{offset: 372, length: 6, factor: 1/ 3600000, unit: 'varh'};
     points['L2.s2regard'] =          	{offset: 384, length: 4, factor: 1/ 10, unit: 'VA'};
-    points['L2.s2regardcounter'] =   	{offset: 392, length: 8, factor: 1/ 3600000, unit: 'VAh'};
+    points['L2.s2regardcounter'] =   	{offset: 392, length: 6, factor: 1/ 3600000, unit: 'VAh'};
     points['L2.s2surplus'] =         	{offset: 404, length: 4, factor: 1/ 10, unit: 'VA'};
-    points['L2.s2surpluscounter'] =  	{offset: 412, length: 8, factor: 1/ 3600000, unit: 'VAh'};
+    points['L2.s2surpluscounter'] =  	{offset: 412, length: 6, factor: 1/ 3600000, unit: 'VAh'};
     points['L2.cosphi2'] =           	{offset: 440, length: 4, factor: 1/ 1000, unit: 'Φ'};
     points['L2.amperage'] =           	{offset: 424, length: 4, factor: 1/ 1000, unit: 'A'};
     points['L2.v2'] =           	{offset: 432, length: 4, factor: 1/ 1000, unit: 'V'};
@@ -164,18 +164,18 @@ if (adapter.config.ext && adapter.config.ext === true) {
  };
  if (adapter.config.L3 && adapter.config.L3 === true) {
     points['L3.p3regard'] =              {offset: 448, length: 4, factor: 1/ 10, unit: 'Watt'};
-    points['L3.p3regardcounter'] =       {offset: 456, length: 8, factor: 1/ 3600000, unit: 'kWh'};
+    points['L3.p3regardcounter'] =       {offset: 456, length: 6, factor: 1/ 3600000, unit: 'kWh'};
     points['L3.p3surplus'] =             {offset: 468, length: 4, factor: 1/ 10, unit: 'Watt'};
-    points['L3.p3surpluscounter'] =      {offset: 476, length: 8, factor: 1/ 3600000, unit: 'kWh'};
+    points['L3.p3surpluscounter'] =      {offset: 476, length: 6, factor: 1/ 3600000, unit: 'kWh'};
 if (adapter.config.ext && adapter.config.ext === true) {   
     points['L3.q3regard'] =              {offset: 488, length: 4, factor: 1/ 10, unit: 'var'};
-    points['L3.q3regardcounter'] =       {offset: 496, length: 8, factor: 1/ 3600000, unit: 'varh'};
+    points['L3.q3regardcounter'] =       {offset: 496, length: 6, factor: 1/ 3600000, unit: 'varh'};
     points['L3.q3surplus'] =             {offset: 508, length: 4, factor: 1/ 10, unit: 'var'};
-    points['L3.q3surpluscounter'] =      {offset: 516, length: 8, factor: 1/ 3600000, unit: 'varh'};
+    points['L3.q3surpluscounter'] =      {offset: 516, length: 6, factor: 1/ 3600000, unit: 'varh'};
     points['L3.s3regard'] =              {offset: 528, length: 4, factor: 1/ 10, unit: 'VA'};
-    points['L3.s3regardcounter'] =       {offset: 536, length: 8, factor: 1/ 3600000, unit: 'VAh'};
+    points['L3.s3regardcounter'] =       {offset: 536, length: 6, factor: 1/ 3600000, unit: 'VAh'};
     points['L3.s3surplus'] =             {offset: 548, length: 4, factor: 1/ 10, unit: 'VA'};
-    points['L3.s3surpluscounter'] =      {offset: 556, length: 8, factor: 1/ 3600000, unit: 'VAh'};
+    points['L3.s3surpluscounter'] =      {offset: 556, length: 6, factor: 1/ 3600000, unit: 'VAh'};
     points['L3.cosphi3'] =               {offset: 584, length: 4, factor: 1/ 1000, unit: 'Φ'};
     points['L3.amperage'] =           	 {offset: 568, length: 4, factor: 1/ 1000, unit: 'A'};
     points['L3.v3'] =           	 {offset: 576, length: 4, factor: 1/ 1000, unit: 'V'};
